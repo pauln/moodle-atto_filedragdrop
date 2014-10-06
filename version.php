f7fdfc97368d7e9fc3297c43bff8a2b450f11c85
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2014-10-06 15:56:16 +1300

    Bump required Moodle version to 2.7 (2014041100)

--- version.php
+++ version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2014031800;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2013110500;        // Requires this Moodle version.
+$plugin->requires  = 2014041100;        // Requires this Moodle version.
 $plugin->component = 'atto_imagedragdrop';  // Full name of the plugin (used for diagnostics).
