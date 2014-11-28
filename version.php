15245944ecd2f0645df472e65f4e9d87c5e4046f
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2014-11-28 16:46:38 +1300

    Version bump to 1.0.2

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version   = 2014031801;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2014031802;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2014041100;        // Requires this Moodle version.
-$plugin->release   = '1.0.1';
+$plugin->release   = '1.0.2';
 $plugin->component = 'atto_imagedragdrop';  // Full name of the plugin (used for diagnostics).
