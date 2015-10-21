bdd0a271c803cba7b021f019fcbaee5fc3a21be9
Paul Nicholls <paul.nicholls@canterbury.ac.nz>
2015-10-21 16:17:43 +1300

    Bring coding style more in line with Moodle guidelines
    Drag/drop handler is now a named method, and variable declarations happen
    in combined var blocks at the start of scopes.

--- yui/build/moodle-atto_filedragdrop-button/moodle-atto_filedragdrop-button.js
+++ yui/build/moodle-atto_filedragdrop-button/moodle-atto_filedragdrop-button.js
@@ -65,105 +65,111 @@ Y.namespace('M.atto_filedragdrop').Button = Y.Base.create('button', Y.M.editor_a
      * @method initializer
      */
     initializer : function() {
-        var self = this;
-        var host = this.get('host');
-        var template = Y.Handlebars.compile(LINKTEMPLATE);
-        var imgtemplate = Y.Handlebars.compile(IMAGETEMPLATE);
-        this.editor.on('drop', function(e) {
-            host.saveSelection();
-            e = e._event;
-            // Only handle the event if an image file was dropped in.
-            var handlesDataTransfer = (e.dataTransfer && e.dataTransfer.files && e.dataTransfer.files.length);
-            if (handlesDataTransfer && !/^image\//.test(e.dataTransfer.files[0].type)) {
-                e.preventDefault();
-                e.stopPropagation();
-                var options = host.get('filepickeroptions').link;
-                var savepath = (options.savepath === undefined) ? '/' : options.savepath;
-                var formData = new FormData();
+        this.editor.on('drop', this._handleDragDrop, this);
+    },
 
-                formData.append('repo_upload_file', e.dataTransfer.files[0]);
-                formData.append('itemid', options.itemid);
-                // List of repositories is an object rather than an array.  This makes iteration more awkward.
-                var keys = Object.keys(options.repositories);
-                for (var i=0; i<keys.length; i++) {
-                    if (options.repositories[keys[i]].type === 'upload') {
-                        formData.append('repo_id', options.repositories[keys[i]].id);
-                        break;
-                    }
+    _handleDragDrop : function(e) {
+        var self = this,
+            host = this.get('host'),
+            template = Y.Handlebars.compile(LINKTEMPLATE),
+            imgtemplate = Y.Handlebars.compile(IMAGETEMPLATE);
+        host.saveSelection();
+        e = e._event;
+        // Only handle the event if an image file was dropped in.
+        var handlesDataTransfer = (e.dataTransfer && e.dataTransfer.files && e.dataTransfer.files.length);
+        if (handlesDataTransfer && !/^image\//.test(e.dataTransfer.files[0].type)) {
+            var options = host.get('filepickeroptions').link,
+                savepath = (options.savepath === undefined) ? '/' : options.savepath,
+                formData = new FormData(),
+                timestamp = 0,
+                uploadid = "",
+                xhr = new XMLHttpRequest(),
+                imagehtml = "",
+                keys = Object.keys(options.repositories);
+
+            e.preventDefault();
+            e.stopPropagation();
+            formData.append('repo_upload_file', e.dataTransfer.files[0]);
+            formData.append('itemid', options.itemid);
+
+            for (var i=0; i<keys.length; i++) {
+                if (options.repositories[keys[i]].type === 'upload') {
+                    formData.append('repo_id', options.repositories[keys[i]].id);
+                    break;
                 }
-                formData.append('env', options.env);
-                formData.append('sesskey', M.cfg.sesskey);
-                formData.append('client_id', options.client_id);
-                formData.append('savepath', savepath);
-                formData.append('ctx_id', options.context.id);
+            }
+            formData.append('env', options.env);
+            formData.append('sesskey', M.cfg.sesskey);
+            formData.append('client_id', options.client_id);
+            formData.append('savepath', savepath);
+            formData.append('ctx_id', options.context.id);
 
-                // Insert spinner as a placeholder.
-                var timestamp = new Date().getTime();
-                var uploadid = 'moodlefile_' + Math.round(Math.random()*100000)+'-'+timestamp;
+            // Insert spinner as a placeholder.
+            timestamp = new Date().getTime();
+            uploadid = 'moodlefile_' + Math.round(Math.random()*100000)+'-'+timestamp;
+            host.focus();
+            host.restoreSelection();
+            imagehtml = imgtemplate({
+                url: M.util.image_url("i/loading_small", 'moodle'),
+                alt: M.util.get_string('uploading', COMPONENTNAME),
+                id: uploadid
+            });
+            host.insertContentAtFocusPoint(imagehtml);
+            self.markUpdated();
 
-                host.focus();
-                host.restoreSelection();
-                var imagehtml = imgtemplate({
-                    url: M.util.image_url("i/loading_small", 'moodle'),
-                    alt: M.util.get_string('uploading', COMPONENTNAME),
-                    id: uploadid
-                });
-                host.insertContentAtFocusPoint(imagehtml);
-                self.markUpdated();
+            xhr.onreadystatechange = function() {
+                var placeholder = self.editor.one('#' + uploadid),
+                    result,
+                    file,
+                    newhtml,
+                    newtag;
 
-                var xhr = new XMLHttpRequest();
-
-                xhr.onreadystatechange = function() {
-                    if (xhr.readyState === 4) {
-                        var placeholder = self.editor.one('#' + uploadid);
-                        if (xhr.status === 200) {
-                            var result = JSON.parse(xhr.responseText);
-                            if (result) {
-                                if (result.error) {
-                                    if (placeholder) {
-                                        placeholder.remove(true);
-                                    }
-                                    return new M.core.ajaxException(result);
-                                }
-
-                                var file = result;
-                                if (result.event && result.event === 'fileexists') {
-                                    // A file with this name is already in use here - rename to avoid conflict.
-                                    // Chances are, it's a different file (stored in a different folder on the user's computer).
-                                    // If the user wants to reuse an existing file, they can copy/paste the link within the editor.
-                                    file = result.newfile;
-                                }
-
-                                // Replace placeholder with actual link.
-                                var newhtml = template({
-                                    url: file.url,
-                                    text: file.file || file.filename
-                                });
-                                var newtag = Y.Node.create(newhtml);
+                if (xhr.readyState === 4) {
+                    if (xhr.status === 200) {
+                        result = JSON.parse(xhr.responseText);
+                        if (result) {
+                            if (result.error) {
                                 if (placeholder) {
-                                    placeholder.replace(newtag);
-                                } else {
-                                    self.editor.appendChild(newtag);
+                                    placeholder.remove(true);
                                 }
-                                self.markUpdated();
+                                return new M.core.ajaxException(result);
                             }
-                        } else {
-                            Y.use('moodle-core-notification-alert', function() {
-                                new M.core.alert({message: M.util.get_string('servererror', 'moodle')});
+
+                            file = result;
+                            if (result.event && result.event === 'fileexists') {
+                                // A file with this name is already in use here - rename to avoid conflict.
+                                // Chances are, it's a different file (stored in a different folder on the user's computer).
+                                // If the user wants to reuse an existing file, they can copy/paste the link within the editor.
+                                file = result.newfile;
+                            }
+
+                            // Replace placeholder with actual link.
+                            newhtml = template({
+                                url: file.url,
+                                text: file.file || file.filename
                             });
+                            newtag = Y.Node.create(newhtml);
                             if (placeholder) {
-                                placeholder.remove(true);
+                                placeholder.replace(newtag);
+                            } else {
+                                self.editor.appendChild(newtag);
                             }
+                            self.markUpdated();
+                        }
+                    } else {
+                        Y.use('moodle-core-notification-alert', function() {
+                            new M.core.alert({message: M.util.get_string('servererror', 'moodle')});
+                        });
+                        if (placeholder) {
+                            placeholder.remove(true);
                         }
                     }
-                };
-
-                // Send the AJAX call.
-                xhr.open("POST", M.cfg.wwwroot + '/repository/repository_ajax.php?action=upload', true);
-                xhr.send(formData);
-                return false;
-            }
-        }, this);
+                }
+            };
+            xhr.open("POST", M.cfg.wwwroot + '/repository/repository_ajax.php?action=upload', true);
+            xhr.send(formData);
+            return false;
+        }
     }
 
 });
